c739cd4ddcabb3796d9508b2616aa2670ced34d8
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-11 15:53:08 +0700

    Create function helloWorld()

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func helloWorld() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World!", preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 
 }
